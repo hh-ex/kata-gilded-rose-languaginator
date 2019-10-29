e33e99853a036fa7df14bd07d226c7e71e601f99
Benjamin Weber <mail@bwe.im>
2019-10-29 20:36:23 +0100

    start refactoring: update_conjured_item, update_item, update_sell_in

--- gilded_rose/lib/gilded_rose.ex
+++ gilded_rose/lib/gilded_rose.ex
@@ -7,6 +7,37 @@ defmodule GildedRose do
     Enum.map(items, &update_item/1)
   end
 
+  # item.name
+  # item.quality
+  # item.sell_in
+
+  # quality
+  # - incr
+  # - decr
+
+  # quality
+  # - clip: min: 0, max: 50
+  
+  defp update_sell_in(item) do
+    %{item | sell_in: item.sell_in - 1}
+  end
+
+  def update_item(%Item{name: "Sulfuras, Hand of Ragnaros"} = item), do: item
+
+  def update_item(%Item{name: "Conjured Item"} = item), do: update_conjured_item(item)
+
+  defp update_conjured_item(item) do
+    item |> update_quality(-2) |> update_sell_in
+  end
+
+  defp update_quality(%Item{sell_in: sell_in} = item, n) when sell_in > 0 do
+    %{item | quality: item.quality + n |> min(50) |> max(0)}
+  end
+
+  defp update_quality(item, n) do
+    %{item | quality: item.quality + 2*n |> min(50) |> max(0)}
+  end
+
   def update_item(item) do
     item = cond do
       item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" ->
