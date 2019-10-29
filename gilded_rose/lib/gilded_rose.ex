452cab2a2134fd0c66710096d614dea1fe73d03b
Benjamin Weber <mail@bwe.im>
2019-10-29 21:11:04 +0100

    format

--- gilded_rose/lib/gilded_rose.ex
+++ gilded_rose/lib/gilded_rose.ex
@@ -17,7 +17,7 @@ defmodule GildedRose do
 
   # quality
   # - clip: min: 0, max: 50
-  
+
   defp update_sell_in(item) do
     %{item | sell_in: item.sell_in - 1}
   end
@@ -47,11 +47,11 @@ defmodule GildedRose do
   end
 
   defp update_quality(%Item{sell_in: sell_in} = item, n) when sell_in > 0 do
-    %{item | quality: item.quality + n |> min(50) |> max(0)}
+    %{item | quality: (item.quality + n) |> min(50) |> max(0)}
   end
 
   defp update_quality(item, n) do
-    %{item | quality: item.quality + 2*n |> min(50) |> max(0)}
+    %{item | quality: (item.quality + 2 * n) |> min(50) |> max(0)}
   end
 
   def update_item(%Item{name: "Backstage passes to a TAFKAL80ETC concert"} = item) do
@@ -66,7 +66,7 @@ defmodule GildedRose do
 
   def update_item(%Item{name: "Conjured Item"} = item), do: update_conjured_item(item)
 
-def update_item(item) do
-  item |> update_quality(-1) |> update_sell_in()
-end
+  def update_item(item) do
+    item |> update_quality(-1) |> update_sell_in()
+  end
 end
